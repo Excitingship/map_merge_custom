4c7249dffe9936a7b36f5931e50221fdc6bd84fd
Excitingship <13353190922@163.com>
2022-01-13 17:10:21 +0800

    fix bug and now it can work in real sence, maybe still have some bugs which would cause collapsion.

--- src/map_merge.cpp
+++ src/map_merge.cpp
@@ -160,13 +160,13 @@ void MapMerge::mapMerging()
         std::cout << "cos: " << cos(tf::getYaw(subscription.initial_pose.rotation)) << std::endl;
         geometry_msgs::Transform tfMap2RobotMapTL;
         tfMap2RobotMapTL.rotation = subscription.initial_pose.rotation;
-        tfMap2RobotMapTL.translation.x = \
-          (subscription.initial_pose.translation.x - ( \
+        tfMap2RobotMapTL.translation.x -= \
+          (subscription.initial_pose.translation.x + ( \
           cos(tf::getYaw(subscription.initial_pose.rotation))*(subscription.readonly_map->info.origin.position.x) + \
           sin(tf::getYaw(subscription.initial_pose.rotation))*(subscription.readonly_map->info.origin.position.y)))/subscription.readonly_map->info.resolution;
         std::cout << "tfMap2MapCenter.translation.x = " << tfMap2RobotMapTL.translation.x << std::endl;
-        tfMap2RobotMapTL.translation.y = \
-          (subscription.initial_pose.translation.y - ( \
+        tfMap2RobotMapTL.translation.y -= \
+          (subscription.initial_pose.translation.y + ( \
           cos(tf::getYaw(subscription.initial_pose.rotation))*(subscription.readonly_map->info.origin.position.y)- \
           sin(tf::getYaw(subscription.initial_pose.rotation))*(subscription.readonly_map->info.origin.position.x)))/subscription.readonly_map->info.resolution;
         std::cout << "tfMap2MapCenter.translation.y = " << tfMap2RobotMapTL.translation.y << std::endl;
